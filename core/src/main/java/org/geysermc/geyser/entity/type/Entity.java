f26562b6064085952aad3c034f902d0fec0f15ad
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2024-04-03 22:05:47 -0700

    Update Entity.java

--- core/src/main/java/org/geysermc/geyser/entity/type/Entity.java
+++ core/src/main/java/org/geysermc/geyser/entity/type/Entity.java
@@ -126,6 +126,9 @@ public class Entity implements GeyserEntity {
     @Setter(AccessLevel.PROTECTED) // For players
     private boolean flagsDirty = false;
 
+    @Getter
+    public long leashHolderBedrockId;
+
     public Entity(GeyserSession session, int entityId, long geyserId, UUID uuid, EntityDefinition<?> definition, Vector3f position, Vector3f motion, float yaw, float pitch, float headYaw) {
         this.session = session;
 
@@ -160,8 +163,14 @@ public class Entity implements GeyserEntity {
         setFlag(EntityFlag.CAN_CLIMB, true);
         // Let the Java server (or us) supply all sounds for an entity
         setClientSideSilent();
+        setLeashHolderBedrockId(-1);
     }
 
+    public void setLeashHolderBedrockId(long bedrockId) {
+        this.leashHolderBedrockId = bedrockId;
+        dirtyMetadata.put(EntityData.LEASH_HOLDER_EID, bedrockId); 
+    }
+    
     protected void setClientSideSilent() {
         setFlag(EntityFlag.SILENT, true);
     }
